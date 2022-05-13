2f24050e65df65524b97e2b2af10aec1774eca40
Dula <dula.xyz@gmail.com>
2022-05-13 16:37:01 +0300

    Deleted test file for now and changed index.js to app.js as well as updates on JWT to use UID and username

--- jwt.js
+++ jwt.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const JWT = require("jsonwebtoken");
 const KEY = process.env.JWT_SECRET;
 
-// take UID and return JWT
-const createJWT = uid => JWT.sign(uid, KEY);
+// take {UID, username} and return JWT
+const createJWT = (uid, username) => JWT.sign({ uid, username }, KEY);
 
-// take JWT and return UID || Error
+// take JWT and return {UID, username} || Error
 const verifyJWT = jwt => {
 	const response = JWT.verify(jwt, KEY, (err, uid) => {
 		if (err) {
