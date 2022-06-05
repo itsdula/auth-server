c73cc474d11c2ee33f7b9932fa04826196db7c9c
Dula <dula.xyz@gmail.com>
2022-06-05 14:47:30 +0300

    routes are refactored into an express.Router object

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { serverError, invalidInput, invalidCredentials, forbidden, userNotFound } = require("./ErrMsgs");
-const { createUser, login, removeUser, authorize } = require("./auth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,41 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// Create user
-app.post("/createUser", async function (req, res) {
-	const { username, password } = req?.body;
-	try {
-		const jwt = await createUser(username, password);
-		jwt ? res.status(201).json(jwt) : res.status(400).json({ message: invalidInput });
-	} catch (err) {
-		console.err(err);
-		res.status(500).json({ message: serverError });
-	}
-});
-
-// Login
-app.post("/login", function (req, res) {
-	const { username, password } = req?.body;
-	const jwt = login(username, password);
-
-	jwt ? res.status(200).json(jwt) : res.status(400).json({ message: invalidCredentials });
-});
-
-// Authorize
-app.get("/auth", function (req, res) {
-	const { jwt } = req?.body;
-
-	const user = authorize(jwt);
-
-	user ? res.status(200).json(user) : res.status(403).json({ message: forbidden });
-});
-
-app.delete("/removeUser", function (req, res) {
-	const { jwt } = req?.body;
-	const username = removeUser(jwt);
-	username
-		? res.status(200).json({ message: `${username} is removed.` })
-		: res.status(400).json({ message: userNotFound });
-});
+// User the Auth routes
+app.use("/auth", require("./routers/AuthRouter"));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
---
+++ routers/AuthRouter.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const { createUser, login, removeUser, authorize } = require("../auth");
+const { serverError, invalidInput, invalidCredentials, forbidden, userNotFound } = require("../ErrMsgs");
+
+// Create user
+router.post("/createUser", async function (req, res) {
+	const { username, password } = req?.body;
+	try {
+		const jwt = await createUser(username, password);
+		jwt ? res.status(201).json({ jwt, username }) : res.status(400).json({ message: invalidInput });
+	} catch (err) {
+		console.err(err);
+		res.status(500).json({ message: serverError });
+	}
+});
+
+// Login
+router.post("/login", function (req, res) {
+	const { username, password } = req?.body;
+	const jwt = login(username, password);
+
+	if (jwt) {
+		console.log(`\x1b[33m${username} is logged in`);
+		res.status(200).json({ jwt, username });
+	} else {
+		console.log("No JWT");
+		res.status(400).json({ message: invalidCredentials });
+	}
+});
+
+// Authorize
+router.get("/auth", function (req, res) {
+	const data = req?.body;
+	console.log("HERE", data);
+
+	// const user = authorize(jwt);
+	// console.log(user);
+
+	const user = null;
+	user ? res.status(200).json(user) : res.status(403).json({ message: forbidden });
+});
+
+router.delete("/removeUser", function (req, res) {
+	const { jwt } = req?.body;
+	const username = removeUser(jwt);
+	username
+		? res.status(200).json({ message: `${username} is removed.` })
+		: res.status(400).json({ message: userNotFound });
+});
+
+module.exports = router;
